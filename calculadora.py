9f62fca52676a83563f4307abced7e0529a0d312
João Carlos Vieira do Nascimento <x507493@rede.sp>
2026-03-10 10:49:24 -0300

    Resolve conflito pelo terminal

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,3 @@
 # Calculadora Básica
-<<<<<<< HEAD
+def soma(a, b): return a+b
 def subtracao(a, b): return a - b
-=======
-def soma(a, b): return a + b
-# Desenvolvedor B: Implementar a função de subtração aqui
->>>>>>> main
